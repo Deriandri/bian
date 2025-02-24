0bbd385fc57b28c99975e54b19a3919e2d0dbacc
Deriandri <deriandriananda82@gmail.com>
2025-02-24 10:34:27 +0700

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -5,7 +5,7 @@ file_path="/etc/handeling"
 # Cek apakah file ada
 if [ ! -f "$file_path" ]; then
     # Jika file tidak ada, buat file dan isi dengan dua baris
-    echo -e "cloudvpn premium server\nBLUE" | sudo tee "$file_path" > /dev/null
+    echo -e "ALWAVPN PREMIUM SERVER\nYellow" | sudo tee "$file_path" > /dev/null
     echo "File '$file_path' berhasil dibuat."
 else
     # Jika file ada, cek apakah isinya kosong
